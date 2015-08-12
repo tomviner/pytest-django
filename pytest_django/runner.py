07853b1ec94826fe0b027364bad0b381fb4eb8a6
Tom Viner <tom.viner@hogarthww.com>
2015-08-12 19:00:12 +0100

    simplify runner

--- pytest_django/runner.py
+++ pytest_django/runner.py
@@ -9,45 +9,23 @@ class PyTestRunner(object):
     Runs py.test to discover and run tests.
     """
     option_list = (
-        make_option('-t', '--top-level-directory',
-                    action='store', dest='top_level', default=None,
-                    help='Top level of project for unittest discovery.'),
-        make_option('-p', '--pattern', action='store', dest='pattern',
-                    default="test*.py",
-                    help='The test matching pattern. Defaults to test*.py.'),
         make_option('-k', '--keepdb',
                     action='store_true', dest='keepdb', default=False,
                     help='Preserves the test DB between runs.'),
     )
 
-    def __init__(self, pattern=None, top_level=None, verbosity=1,
-                 interactive=True, failfast=False, keepdb=False, **kwargs):
-        if pattern and pattern != 'test*.py':
-            # TODO: implement
-            raise NotImplementedError('Testing with a file pattern is not '
-                                      'implemented.')
-        if top_level is not None:
-            # TODO: implement
-            raise NotImplementedError('Specifying the top_level is not '
-                                      'implemented.')
+    def __init__(self, verbosity=1, interactive=True, failfast=False,
+                 keepdb=False, settings=None, **kwargs):
         self.verbosity = verbosity
         self.interactive = interactive
         self.failfast = failfast
         self.keepdb = keepdb
+        self.ds = settings or os.environ['DJANGO_SETTINGS_MODULE']
 
-        # TODO: Make this an option
-        self.ds = os.environ['DJANGO_SETTINGS_MODULE']
-
-    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+    def run_tests(self, test_labels):
         """
         Run py.test and returns the exitcode.
         """
-        if test_labels:
-            # TODO: implement
-            raise NotImplementedError('test_labels is not implemented.')
-        if extra_tests is not None:
-            # TODO: implement
-            raise NotImplementedError('extra_tests is not implemented.')
 
         # Translate arguments
         argv = ['--ds', self.ds]
@@ -60,5 +38,5 @@ class PyTestRunner(object):
         if self.keepdb:
             argv.append('--nomigrations')
 
-        # Run py.test
+        argv.extend(test_labels)
         return pytest.main(argv)
